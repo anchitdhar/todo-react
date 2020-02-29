3c74b4ae34cc12728059847287ebbc4cfca4cc8f
anchitdhar <anchitdhar@yahoo.com>
2020-03-01 04:27:05 +0530

    first commit

---
+++ src/components/Checkbox.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function Checkbox() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p> A</p>
+
+            <input type="checkbox" />
+            <p> B</p>
+
+            <input type="checkbox" />
+            <p> C</p>
+
+            <input type="checkbox" />
+            <p> D</p>
+
+            <input type="checkbox" />
+            <p> E</p>
+        </div>
+    )
+}
+
+export default Checkbox
